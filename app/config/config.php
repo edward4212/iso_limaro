5da67dfbbe0470189c45752a953d38f7fbe93a2b
edward izquierdo <61303005+edward4212@users.noreply.github.com>
2026-02-24 10:21:03 -0500

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+// Config general
+define('APP_NAME', 'LIMARO ISO');
+define('APP_ENV', 'local');
+define('APP_URL', 'https://iso.limaro.cloud/'); // ajusta a tu entorno
+
+// Zona horaria
+date_default_timezone_set('America/Bogota');
