4df945437832d5ff1ee2e80822b2547ee9004655
edward izquierdo <61303005+edward4212@users.noreply.github.com>
2026-02-24 10:24:30 -0500

    Delete app directory

--- app/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Config general
-define('APP_NAME', 'LIMARO ISO');
-define('APP_ENV', 'local');
-define('APP_URL', 'https://iso.limaro.cloud/'); // ajusta a tu entorno
-
-// Zona horaria
-date_default_timezone_set('America/Bogota');
